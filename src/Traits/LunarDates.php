375371012900364dc40c34a6b9915032c139d85a
Cooper <myxiaoao@gmail.com>
2022-04-24 09:39:54 +0800

    macro check

--- src/Traits/LunarDates.php
+++ src/Traits/LunarDates.php
@@ -9,105 +9,131 @@ trait LunarDates
 {
     public function registerLunarDates(): void
     {
-        // 阳历转换为阴历
-        Carbon::macro('getLunar', function () {
-            return Lunar::solar2lunar($this->year, $this->month, $this->day, $this->hour);
-        });
+        if (!Carbon::hasMacro('getLunar')) {
+            // 阳历转换为阴历
+            Carbon::macro('getLunar', function () {
+                return Lunar::solar2lunar($this->year, $this->month, $this->day, $this->hour);
+            });
+        }
 
-        // 获取生肖
-        Carbon::macro('getYearZodiac', function () {
-            return Lunar::getYearZodiac($this->lunar->year);
-        });
+        if (!Carbon::hasMacro('getYearZodiac')) {
+            // 获取生肖
+            Carbon::macro('getYearZodiac', function () {
+                return Lunar::getYearZodiac($this->lunar->year);
+            });
+        }
 
-        // 获取干支纪年
-        Carbon::macro('getLunarYearName', function () {
-            return Lunar::getLunarYearName($this->lunar->year);
-        });
+        if (!Carbon::hasMacro('getLunarYearName')) {
+            // 获取干支纪年
+            Carbon::macro('getLunarYearName', function () {
+                return Lunar::getLunarYearName($this->lunar->year);
+            });
+        }
 
-        // 春节
-        Carbon::macro('isChineseNewYearDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isChineseNewYearDay')) {
+            // 春节
+            Carbon::macro('isChineseNewYearDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 1 && $this->lunar->day === 1;
-        });
+                return $this->lunar->month === 1 && $this->lunar->day === 1;
+            });
+        }
 
-        // 元宵节
-        Carbon::macro('isFirstFullMoonDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isFirstFullMoonDay')) {
+            // 元宵节
+            Carbon::macro('isFirstFullMoonDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 1 && $this->lunar->day === 15;
-        });
+                return $this->lunar->month === 1 && $this->lunar->day === 15;
+            });
+        }
 
-        // 端午节
-        Carbon::macro('isDragonBoatDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isDragonBoatDay')) {
+            // 端午节
+            Carbon::macro('isDragonBoatDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 5 && $this->lunar->day === 5;
-        });
+                return $this->lunar->month === 5 && $this->lunar->day === 5;
+            });
+        }
 
-        // 七夕
-        Carbon::macro('isDoubleSeventhDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isDoubleSeventhDay')) {
+            // 七夕
+            Carbon::macro('isDoubleSeventhDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 7 && $this->lunar->day === 7;
-        });
+                return $this->lunar->month === 7 && $this->lunar->day === 7;
+            });
+        }
 
-        // 中秋节
-        Carbon::macro('isMidAutumnDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isMidAutumnDay')) {
+            // 中秋节
+            Carbon::macro('isMidAutumnDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 8 && $this->lunar->day === 15;
-        });
+                return $this->lunar->month === 8 && $this->lunar->day === 15;
+            });
+        }
 
-        // 重阳节
-        Carbon::macro('isDoubleNinthDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isDoubleNinthDay')) {
+            // 重阳节
+            Carbon::macro('isDoubleNinthDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 9 && $this->lunar->day === 9;
-        });
+                return $this->lunar->month === 9 && $this->lunar->day === 9;
+            });
+        }
 
-        // 腊八节
-        Carbon::macro('isLabaDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isLabaDay')) {
+            // 腊八节
+            Carbon::macro('isLabaDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 12 && $this->lunar->day === 8;
-        });
+                return $this->lunar->month === 12 && $this->lunar->day === 8;
+            });
+        }
 
-        // 北方小年
-        Carbon::macro('isNorthLittleNewYearDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isNorthLittleNewYearDay')) {
+            // 北方小年
+            Carbon::macro('isNorthLittleNewYearDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 12 && $this->lunar->day === 23;
-        });
+                return $this->lunar->month === 12 && $this->lunar->day === 23;
+            });
+        }
 
-        // 南方小年
-        Carbon::macro('isSouthLittleNewYearDay', function () {
-            if ($this->lunar->isLeapMonth) {
-                return false;
-            }
+        if (!Carbon::hasMacro('isSouthLittleNewYearDay')) {
+            // 南方小年
+            Carbon::macro('isSouthLittleNewYearDay', function () {
+                if ($this->lunar->isLeapMonth) {
+                    return false;
+                }
 
-            return $this->lunar->month === 12 && $this->lunar->day === 24;
-        });
+                return $this->lunar->month === 12 && $this->lunar->day === 24;
+            });
+        }
 
-        // 除夕
-        Carbon::macro('isChineseNewYearEve', function () {
-            return $this->addDay()->isChineseNewYearDay();
-        });
+        if (!Carbon::hasMacro('isChineseNewYearEve')) {
+            // 除夕
+            Carbon::macro('isChineseNewYearEve', function () {
+                return $this->addDay()->isChineseNewYearDay();
+            });
+        }
     }
 }
--- src/Traits/SolarDates.php
+++ src/Traits/SolarDates.php
@@ -8,64 +8,88 @@ trait SolarDates
 {
     public function registerSolarDates(): void
     {
-        // 元旦
-        Carbon::macro('isNewYearsDay', function () {
-            return $this->month === 1 && $this->day === 1;
-        });
+        if (!Carbon::hasMacro('isNewYearsDay')) {
+            // 元旦
+            Carbon::macro('isNewYearsDay', function () {
+                return $this->month === 1 && $this->day === 1;
+            });
+        }
 
-        // 情人节
-        Carbon::macro('isValentinesDay', function () {
-            return $this->month === 2 && $this->day === 14;
-        });
+        if (!Carbon::hasMacro('isValentinesDay')) {
+            // 情人节
+            Carbon::macro('isValentinesDay', function () {
+                return $this->month === 2 && $this->day === 14;
+            });
+        }
 
-        // 妇女节
-        Carbon::macro('isWomenDay',function (){
-            return $this->month === 3 && $this->day === 8;
-        });
+        if (!Carbon::hasMacro('isWomenDay')) {
+            // 妇女节
+            Carbon::macro('isWomenDay', function () {
+                return $this->month === 3 && $this->day === 8;
+            });
+        }
 
-        // 植树节
-        Carbon::macro('isArborDay', function () {
-            return $this->month === 3 && $this->day === 12;
-        });
+        if (!Carbon::hasMacro('isArborDay')) {
+            // 植树节
+            Carbon::macro('isArborDay', function () {
+                return $this->month === 3 && $this->day === 12;
+            });
+        }
 
-        // 愚人节
-        Carbon::macro('isAprilFoolsDay', function () {
-            return $this->month === 4 && $this->day === 1;
-        });
+        if (!Carbon::hasMacro('isAprilFoolsDay')) {
+            // 愚人节
+            Carbon::macro('isAprilFoolsDay', function () {
+                return $this->month === 4 && $this->day === 1;
+            });
+        }
 
-        // 劳动节
-        Carbon::macro('isLabourDay',function (){
-            return $this->month === 5 && $this->day === 1;
-        });
+        if (!Carbon::hasMacro('isLabourDay')) {
+            // 劳动节
+            Carbon::macro('isLabourDay', function () {
+                return $this->month === 5 && $this->day === 1;
+            });
+        }
 
-        // 青年节
-        Carbon::macro('isYouthDay',function (){
-            return $this->month === 5 && $this->day === 4;
-        });
+        if (!Carbon::hasMacro('isYouthDay')) {
+            // 青年节
+            Carbon::macro('isYouthDay', function () {
+                return $this->month === 5 && $this->day === 4;
+            });
+        }
 
-        // 儿童节
-        Carbon::macro('isChildrenDay',function (){
-            return $this->month === 6 && $this->day === 1;
-        });
+        if (!Carbon::hasMacro('isChildrenDay')) {
+            // 儿童节
+            Carbon::macro('isChildrenDay', function () {
+                return $this->month === 6 && $this->day === 1;
+            });
+        }
 
-        // 国庆节
-        Carbon::macro('isNationalDay', function () {
-            return $this->month === 10 && $this->day === 1;
-        });
+        if (!Carbon::hasMacro('isNationalDay')) {
+            // 国庆节
+            Carbon::macro('isNationalDay', function () {
+                return $this->month === 10 && $this->day === 1;
+            });
+        }
 
-        // 万圣节
-        Carbon::macro('isHalloween',function (){
-            return $this->month === 10 && $this->day === 31;
-        });
+        if (!Carbon::hasMacro('isHalloween')) {
+            // 万圣节
+            Carbon::macro('isHalloween', function () {
+                return $this->month === 10 && $this->day === 31;
+            });
+        }
 
-        // 平安夜
-        Carbon::macro('isChristmasEve', function () {
-            return $this->month === 12 && $this->day === 24;
-        });
+        if (!Carbon::hasMacro('isChristmasEve')) {
+            // 平安夜
+            Carbon::macro('isChristmasEve', function () {
+                return $this->month === 12 && $this->day === 24;
+            });
+        }
 
-        // 圣诞节
-        Carbon::macro('isChristmasDay', function () {
-            return $this->month === 12 && $this->day === 25;
-        });
+        if (!Carbon::hasMacro('isChristmasDay')) {
+            // 圣诞节
+            Carbon::macro('isChristmasDay', function () {
+                return $this->month === 12 && $this->day === 25;
+            });
+        }
     }
 }
